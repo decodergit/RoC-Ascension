e44a4dddf612d9d86714b9811f9ff71c70122e0f
decodergit <decodermail@gmail.com>
2022-04-12 11:22:22 +0300

    Добавлены ссылки на видео и страницы документа, соответствующие уровням

--- hunt.py
+++ hunt.py
@@ -19,12 +19,24 @@ for user_name in user_names.keys():
 
 print('''\subsection{Примеры расстановки во втором уровне Охоты за Сокровищами}
 
+В целях обучения можно посмотреть
+\\underline{\href{https://youtu.be/JD1PWm27lHg}{видео}}
+о прохождении первого уровня Охоты за Сокровищами.
+
+Ниже представлены примеры расстановки войск, собранные нашим Альянсом:''')
+
+for fight in range(21, 40):
+    print(f'\\hyperlink{{fight{fight}}}{{{fight}}}, ')
+print('\\hyperlink{fight40}{40}.')
+
+print('''
 \\noindent
 \\begin{longtable}{|c|c|c|}
     \\hline
     Битва & Вариант & Расстановка \\\\\\hline\\endhead''')
 
 for fight in range(21, 41):
+    #print(f'    \\hypertarget{{fight{fight}}}{{}}')
     screens = dict()
     total_images = 0
     for user_name in os.listdir(f'./Устав/parts/media/TreasureHunt/{fight}/'):
@@ -41,10 +53,10 @@ for fight in range(21, 41):
             continue
         if len(images) == 1:
             print(f'''    {fight} & {user_names[user_name]} &
-    \\includegraphics[width=0.5\\linewidth]{{./parts/media/TreasureHunt/{fight}/{user_name}/{images[0]}}} \\\\''')
+    \\hypertarget{{fight{fight}}}{{\\includegraphics[width=0.5\\linewidth]{{./parts/media/TreasureHunt/{fight}/{user_name}/{images[0]}}}}} \\\\''')
         else:
             print(f'''    \multirow{{{total_images}}}{{*}}{{{fight}}} & {user_names[user_name]} &
-    \\includegraphics[width=0.75\\linewidth]{{./parts/media/TreasureHunt/{fight}/{user_name}/{images[0]}}} \\\\''')
+    \\hypertarget{{fight{fight}}}{{\\includegraphics[width=0.75\\linewidth]{{./parts/media/TreasureHunt/{fight}/{user_name}/{images[0]}}}}} \\\\''')
             for image in images[1:]:
                 print(f'''    & {user_names[user_name]} &
     \\includegraphics[width=0.75\\linewidth]{{./parts/media/TreasureHunt/{fight}/{user_name}/{image}}} \\\\''')
