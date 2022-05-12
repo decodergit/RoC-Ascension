d5ecb298119d8a1ed7853744becd420008e7b935
decodergit <decodermail@gmail.com>
2022-05-12 18:24:31 +0300

    Исправлено

--- hack/second.sh
+++ hack/second.sh
@@ -4,4 +4,4 @@ APPNAME=riseofcultures
 #APPNAME=nano
 APPPID=`sudo ps -A | grep ${APPNAME} | awk '{print $1}'`
 echo "${APPNAME} PID is ${APPPID}"
-sudo gdb -p ${APPPID} -q -x second.py -ex="set confirm off" -ex="set logging off" -ex quit
+sudo gdb -p ${APPPID} -q -x second.py -ex="set confirm off" -ex="set pagination off" -ex quit
