7090ac15eb662bbd431584bea7c4e37084e4110a
decodergit <decodermail@gmail.com>
2022-05-17 18:08:01 +0300

    Попытка хака

--- hack/hack.sh
+++ hack/hack.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 rm -f ./mem.dump
+rm -f ./mem.vars
 
 APPNAME=riseofcultures
 APPPID=`sudo ps -A | grep ${APPNAME} | awk '{print $1}'`
 echo "${APPNAME} PID is ${APPPID}"
 sudo gdb -p ${APPPID} -ex="set confirm off" -ex="set pagination off" -q -x hack_gdb.py -ex quit
+./hack.py
 
 while : ;
 do
@@ -18,5 +20,6 @@ do
   if [ "$answer" = "y" ];
   then
     sudo gdb -p ${APPPID} -ex="set confirm off" -ex="set pagination off" -q -x hack_gdb.py -ex quit
+    ./hack.py
   fi
 done
